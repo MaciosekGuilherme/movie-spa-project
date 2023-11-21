d7b0cd8a026f5785007a27d9fc6fe39d486d85d2
MaciosekGuilherme <guilherme.maciosek@hotmail.com>
2023-11-21 14:13:24 -0300

    refatoração
    projeto refatorado para tailwind
    acrescentado botão de feedback (não é enviado ao backend no momento)

---
+++ public/src/Header/Header.ts
@@ -0,0 +1,7 @@
+export const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNzhjYTU5OTE0NTdhZDE2ZDdhZTg0ZmM5MDQ5ODM1NSIsInN1YiI6IjY0OGI4NTdkNDJiZjAxMDBlNDlkMGRhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Gu4R7wC3CQv9h00rFDWQtp5hM1mMhlZ518vVCJWlDHU'
+  }
+};
